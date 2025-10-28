26ce2fbb45a02265e6c0907316d7d390f1211bf7
Victoria <kyerk88@mail.ru>
2025-10-28 20:17:49 +0700

    Создай ветки и напиши операции сложения, вычитания, умножения и деления

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите пункт меню:");
+            Console.WriteLine("1. Ввести А");
+            Console.WriteLine("2. Ввести В");
+            Console.WriteLine("3. Выполнить сложение");
+            Console.WriteLine("4. Выполнить вычитание");
+            Console.WriteLine("5. Выполнить умножение");
+            Console.WriteLine("6. Выполнить деление");
+            Console.WriteLine("0. Выход");
+            string key = Console.ReadLine();
+
+            switch (key)
+            {
+                case "1":
+                    break;
+
+                case "2":
+                    break;
+
+                case "3":
+                    break;
+
+                case "4":
+                    break;
+
+                case "5":
+                    break;
+
+                case "6":
+                    break;
+
+                case "0":
+                    break;
+            }
+        }
+    }
+}
